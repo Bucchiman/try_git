1b5457c8247cb1f24ce093d2053f104e813d09a8
Bucchiman <iwabuchi@fun.bio.keio.ac.jp>
2021-01-05 17:08:13 +0900

    initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*
+ * FileName:     hello
+ * CreatedDate:  2021-01-05 17:02:39 +0900
+ * LastModified: 2021-01-05 17:02:52 +0900
+ */
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World!\n");
+    return 0;
+}
